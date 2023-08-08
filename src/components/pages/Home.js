c7de6373c098638a8f911ab2f908b9690f9a2be1
Wojciech Jaskula <w.jaskula8@gmail.com>
2023-08-08 18:25:58 +0200

    clean-code

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,22 +1,13 @@
 import React from "react";
-import { Container, Form, Button } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function Home() {
-  return (
-    //Q:  dodaj tło do tego diva i zmień kolor tekstu  na biały  w home.css  (w folderze components/pages)  
-    //A:  w home.css dodajemy:  
-
-
-
-    
-
+  return (  
 
     <div>
       <h2>Aplikacja do liczenia punktów w grach planszowych</h2>
     </div>
   );
 }
-
 
 export default Home;
